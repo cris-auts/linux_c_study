84bf4e0354db7410b6c63a4622c80dc685ac4e3f
Cris Gu <cris_auts@hotmail.com>
2019-01-08 16:07:50 +0800

    add uart_test_app

---
+++ linux_uart_test_app/misc.c
@@ -0,0 +1,14 @@
+
+
+
+
+
+int get_max(int a , int b)
+{
+	
+	if (a >= b)
+		return a;
+	else
+		return b;
+	
+}
---
+++ linux_uart_test_app/misc.h
@@ -0,0 +1,9 @@
+
+
+
+#define TEST_A (300)
+#define TEST_B (200)
+
+
+
+int get_max(int a , int b);
